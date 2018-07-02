dbc30821bf53212d7bbef89e7cdc87ac47133aa6
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 17:17:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 def display_board(state)
 line="-----------"
-row_1=" #{state[0]} | #{state[1]} |  #{state[2]} "
-row_2=" #{state[3]} | #{state[4]} |  #{state[5]} "
-row_3=" #{state[6]} | #{state[7]} |  #{state[8]} "
+row_1=" #{state[0]} | #{state[1]} | #{state[2]} "
+row_2=" #{state[3]} | #{state[4]} | #{state[5]} "
+row_3=" #{state[6]} | #{state[7]} | #{state[8]} "
 puts row_1
 puts line
 puts row_2
