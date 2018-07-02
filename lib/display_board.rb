307ece6404ec2480c78165f81e1a88efaba653dd
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 17:15:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+line="-----------"
+row_1=" #{state[0]}  | #{state[1]} |  #{state[2]} "
+puts 
+
+end
