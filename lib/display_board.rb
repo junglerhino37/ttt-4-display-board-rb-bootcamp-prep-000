455dc67c8a9146ac2b8fdaa4c537eb80dedfea5e
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 17:16:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,14 @@
 def display_board(state)
 line="-----------"
 row_1=" #{state[0]}  | #{state[1]} |  #{state[2]} "
-puts 
+row_2=" #{state[3]}  | #{state[4]} |  #{state[5]} "
+row_3=" #{state[6]}  | #{state[7]} |  #{state[8]} "
+puts row_1
+puts line
+puts row_2
+puts line
+puts row_3
+puts line
+
 
 end
